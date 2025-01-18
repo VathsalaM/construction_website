04c86cd3ca32a4194fa9254a085d02a088ddc217
VathsalaM <vathsala@thoughtworks.com>
2025-01-18 22:53:32 +0530

    Try fixing i18n issue

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@
     });
 
     if (window.sessionStorage) {
-        var localLanguage = sessionStorage.getItem('localLanguage', document.documentElement.lang);
+        var localLanguage = sessionStorage.getItem('localLanguage', document.documentElement.lang) || "en";
         fetch(`../i18n/${localLanguage}.json`)
             .then((res) => res.json())
             .then((translation) => {
